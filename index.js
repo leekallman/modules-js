fe9a2d9002cfa7347c77efc31317f7159c3b08de
Lisa Lee <lisaleekallman@gmail.com>
2021-04-01 18:29:52 +0200

    practicing import/export js modules

---
+++ index.js
@@ -0,0 +1,22 @@
+// import { sum, sub } from './calc.js';
+
+// console.log('The Sum is', sum(2, 3));
+// console.log('The Sub is', sub(5, 3));
+
+
+//Importing as a Namespace
+import * as Calc from './calc.js';
+
+console.log('The Sum is', Calc.sum(2, 3));
+console.log('The Sub is', Calc.sub(5, 3));
+
+
+//Import using default as syntax
+// import { default as sayMyName } from './whoami.js';
+
+
+//Import without the curly braces ({ })
+import sayMyName from './whoami.js';
+
+
+console.log(sayMyName("Lisa"));
---
+++ whoami.js
@@ -0,0 +1,9 @@
+const sayMyName = (name) => {
+    return `My name is ${name}`;
+};
+
+export const sayGreeting = greet => {
+    return `Hello ${greet}.`;
+}
+
+export default sayMyName;
